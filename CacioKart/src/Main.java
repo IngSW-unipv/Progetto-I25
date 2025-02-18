1bf902be6f8502f43e189e5a3721279461fad0c7
DAlbardi <davide.albani01@universitadipavia.it>
2025-02-18 16:19:35 +0100

    test per i commit

--- CacioKart/src/Main.java
+++ CacioKart/src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("alba world!");
-        //Heylà
-        //Ciao Luca questo è un test
-        /*
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-        deserunt mollit anim id est laborum.
-         */
+       int i = 0;
+       if(i == 0) {
+           while (i < 10) {
+               i++;
+           }
+       }
     }
 
 }
