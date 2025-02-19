e4f5cd001bba58abee7210257a1a71a99eb82c74
DAlbardi <davide.albani01@universitadipavia.it>
2025-02-19 18:10:44 +0100

    Creazione classe dbConnector

---
+++ CacioKart/src/dbConnector.java
@@ -0,0 +1,85 @@
+import java.sql.*;
+
+/** Classe per istanziare connessioni con il database, eseguire query e restituire risultati
+ * Ver 1.0
+ */
+public class dbConnector {
+    private final static String URL = "jdbc:mysql://localhost:3306/caciokart";
+    private final static String USER = "";
+    private final static String PASSWORD = "";
+    private Connection conn;
+    private dbConnector db;
+
+    public dbConnector() {
+    }
+
+    /** Metodo per aprire la connessione al database.
+     * Utilizzabile solo in questa classe dai metodi per
+     * eseguire le varie query
+     *
+     * @throws SQLException
+     */
+    private void dbOpenConnection() throws SQLException {
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** Metodo per chiudere la connessione al database.
+     * Utilizzabile solo in questa classe dai metodi per
+     * eseguire le varie query
+     *
+     * @throws SQLException
+     */
+    private void dbCloseConnection() throws SQLException {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** Metodo per eseguire query di lettura dati.
+     * ResultSet permette alla classe chiamante di ricevere
+     * i dati richiesti.La presenza del throws e del try-catch
+     * costringe la classe chiamante a gestire un'eventuale eccezione
+     *
+     * @param query
+     * @return
+     * @throws SQLException
+     */
+    public ResultSet executeReturnQuery(String query) throws SQLException {
+        try {
+            db.dbOpenConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            db.dbCloseConnection();
+            return rs;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /** Metodo per eseguire query di scritture dati.
+     * Tramite la stringa in ingresso, il metodo esegue query
+     * che modificano i dati presenti nel database, cancellando
+     * o aggiungendone di nuovi. La presenza del throws e del try-catch
+     * costringe la classe chiamante a gestire un'eventuale eccezione
+     *
+     * @param query
+     * @throws SQLException
+     */
+    public void executeUpdateQuery(String query) throws SQLException {
+        try {
+            db.dbOpenConnection();
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+            db.dbCloseConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
