2a8f58d70367ae88d95da93e643f95a7583b360f
DAlbardi <davide.albani01@universitadipavia.it>
2025-02-20 16:21:03 +0100

    Testing WebConnector

--- CacioKart/src/WebConnector.java
+++ CacioKart/src/WebConnector.java
@@ -10,13 +10,15 @@ public class WebConnector {
             Socket socket = serverSocket.accept(); // Attende connessioni
             System.out.println("Client connesso!");
 
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println("1 2");
-            /*
+            //PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            //out.println("1 2");
+
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String messaggio = in.readLine(); // Legge il messaggio dal client
+            String messaggio = in.readLine();
+
             System.out.println("Messaggio ricevuto: " + messaggio);
-            */
+
+
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
