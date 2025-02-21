4e0e91511ad772d05dadb61f79e04f35a63e3282
Ale <lelow@lol.it>
2025-02-21 15:04:48 +0100

    prova

--- dromokart-WebSite/logic/authenticate.php
+++ dromokart-WebSite/logic/authenticate.php
@@ -18,20 +18,20 @@
     //vengono ricevute due cifre intere separate da uno spazio: la prima è una cifra per un confronto booleano,
     //la seconda serve ad indicare il grado dell'utente e viene salvata nelle variabili di sessione
     while(!feof($socket)){
-        $res = fgets($socket, 3);
+        $logged = fgets($socket, 1);
+        $rnk = fgets($socket, 1);
     }
 
     fclose($socket);
 
-    $str = explode(' ',$res);
     //la seconda cifra indica: 0 utente semplice; 1 meccanico; 2 gestore concessionaria; 3 arbitro; 4 organizzatore;
     //5 proprietario 
-    if(($str[0]) == 0){
+    if($logged == 0){
         header('../index.php');
         die();
     } else{
         $_SESSION['user'] = $username;
-        $_SESSION['rank'] = $str['1'];
+        $_SESSION['rank'] = $rnk;
         header('../profilo.php');
         die();
     }
