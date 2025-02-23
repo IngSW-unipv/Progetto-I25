253c4335b7b1a5329e3eea70c3b0e5ab4767795e
DAlbardi <davide.albani01@universitadipavia.it>
2025-02-23 12:02:58 +0100

    Login e registrazione funzionante

--- CacioKart/src/PHPRequestHandler.java
+++ CacioKart/src/PHPRequestHandler.java
@@ -5,40 +5,31 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class PHPRequestHandler {
+    BufferedReader in;
 
     public PHPRequestHandler() {
     }
 
+    /**La classe riceve stringhe composte in questo modo: *lettera singola* *dati*
+     *La lettera indica la richiesta del client da eseguire
+     *
+     * @param clientSocket
+     */
     public void handleRequests(Socket clientSocket) {
         try {
-            /**La classe si aspetta in entrata una stringa che inizia con un valore ENUM dichiarato
-             * sopra in modo da interpretare la richiesta e decidere a che classe dare la responsabilità
-             * di gestirla
-             */
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String messaggio = in.readLine();
-            System.out.println(messaggio);
             char comando = messaggio.charAt(0);
             messaggio = messaggio.substring(1);
 
             switch (comando) {
                 case 'l':
-                    //Mi aspetto 'l CF password\n'
-                    String[] loginData = messaggio.split(" ");
-                    Persona utente = new Persona(null,null,null,null,null,null);
-                    utente.setcF(loginData[1]);
-                    utente.setPassword(loginData[2]);
-                    utente.login(clientSocket);
-
+                    loginCase(messaggio, clientSocket);
                     break;
 
                 case 'r':
-                    //Mi aspetto 'r Nome Cognome Data CF mail password\n'
-                    String[] socio = messaggio.split(" ");
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                    LocalDate dataNascita = LocalDate.parse(socio[3], formatter);
-                    Socio nuovoUtente = new Socio(socio[1], socio[2], dataNascita, socio[4], socio[5], socio[6]);
-                    nuovoUtente.registrazione(clientSocket);
+                    registerCase(messaggio, clientSocket);
                     break;
 
                 default:
@@ -51,4 +42,28 @@ public class PHPRequestHandler {
         }
 
     }
+
+
+    /**Classe per gestire la logica di login
+     * Prendo il messaggio e lo divido nelle singole informazioni richieste
+     *
+     * @param messaggio
+     * @param clientSocket
+     */
+    private void loginCase(String messaggio, Socket clientSocket) {
+        String[] loginData = messaggio.split(" ");
+        Persona utente = new Persona(null, null, null, null, null, null);
+        utente.setcF(loginData[1]);
+        utente.setPassword(loginData[2]);
+        utente.login(clientSocket);
+    }
+
+    private void registerCase(String messaggio, Socket clientSocket){
+        String[] socio = messaggio.split(" ");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate dataNascita = LocalDate.parse(socio[3], formatter);
+        Socio nuovoUtente = new Socio(socio[1], socio[2], dataNascita, socio[4], socio[5], socio[6]);
+        nuovoUtente.registrazione(clientSocket);
+    }
+
 }
