9c399fd245d92729b3c947858a4987e9f0e66c24
Ale <lelow@lol.it>
2025-03-05 16:52:25 +0100

    fix

--- dromokart-WebSite/logic/requestParts.php
+++ dromokart-WebSite/logic/requestParts.php
@@ -1,6 +1,4 @@
 <?php
-    session_start();
-
     require 'connection.php';
 
     $socket = connectionOpen($address, $port);
@@ -12,10 +10,10 @@
     //i parametri sono: nome del pezzo, quantita`, prezzo
     while(($res = fgets($socket))  != "end"){
         $str = explode($res, ' ');
-        $part[$i][0] = str[0];
-        $part[$i][1] = str[1];
-        $part[$i][2] = str[2];
-        $part[$i][3] = str[3];
+        $part[$i][0] = $str[0];
+        $part[$i][1] = $str[1];
+        $part[$i][2] = $str[2];
+        $part[$i][3] = $str[3];
         $i++;
     }
 
