dbbbf764e10faf44d4f5ec34cef7bca7b637e6b8
Ale <lelow@lol.it>
2025-03-08 10:30:45 +0100

    upd

--- dromokart-WebSite/aggiungiPezzi.php
+++ dromokart-WebSite/aggiungiPezzi.php
@@ -4,45 +4,6 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-<!--
-<style>
-   
-    .product button {
-      padding: 10px 20px;
-      font-size: 16px;
-      cursor: pointer;
-      background-color: green;
-      color: white;
-      border: none;
-      border-radius: 5px;
-    }
-    .product button:hover {
-      background-color: darkgreen;
-    }
-
-    input[type="number"] {
-        width: 80px;
-        padding: 10px;
-        font-size: 18px;
-        border: 2px solid #ddd;
-        border-radius: 10px;
-        outline: none;
-        text-align: center;
-        transition: all 0.3s ease;
-    }
-
-    input[type="number"]:focus {
-        border-color: #4CAF50;
-        box-shadow: 0 0 10px rgba(76, 175, 80, 0.4);
-    }
-
-    input[type="number"]::-webkit-outer-spin-button,
-    input[type="number"]::-webkit-inner-spin-button {
-        -webkit-appearance: inner-spin-button;
-        margin: 0;
-    }
-
-  </style>-->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Aggiungi Pezzi</title>
@@ -77,7 +38,7 @@
             echo '<form method="post" action="logic/insertParts.php">';
             echo '<input type="number" name="quantity" id="quantity" value=0 min=0 required>';
             echo '<input type="hidden" name="part" id="part" value="' .$columns[0] .'" required>';            
-            echo '<button type="submit">Inserisci</button>';
+            echo '<button type="submit">Aggiungi</button>';
             echo '</form>';
             echo '<p>Quantità: ';
             
@@ -93,35 +54,5 @@
         echo '<p>Nessun dato ricevuto.</p>';
       }
     ?>
-<!--
-<div class="container">
-    <-- Primo prodotto --
-    <div class="product-card">
-      <img src="/immagini/motore.png" alt="Motore">
-      <p>Motore 150cc</p>
-      <div class="quantity-controls">
-        <button onclick="updateQuantity(this, -1)">-</button>
-        <span class="quantity">0</span>
-        <button onclick="updateQuantity(this, 1)">+</button>
-      </div>
-    </div>
-    
-    -->
- 
-<!--
-  <script>
-    // Funzione per aggiornare la quantità
-    function updateQuantity(button, delta) {
-      var quantitySpan = button.parentElement.querySelector('.quantity');
-      var currentQuantity = parseInt(quantitySpan.textContent, 10);
-      var newQuantity = currentQuantity + delta;
-      if (newQuantity < 0) {
-        newQuantity = 0; // Impedisce quantità negative
-      }
-      quantitySpan.textContent = newQuantity;
-    }
-  </script>
-    -->
-
 </body>
 </html>
