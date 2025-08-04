83b4eeb74531325083ea650edfe8220deb9bb388
Ale <alessandropiace2013@gmail.com>
2025-08-04 12:23:32 +0200

    fix

--- CacioKart/src/DAO/SocioDAO.java
+++ CacioKart/src/DAO/SocioDAO.java
@@ -16,10 +16,6 @@ import java.util.*;
 
 public class SocioDAO implements InterfacciaSocioDAO{
     private final DBConnector db;
-    private String query;
-    private List<Map<String, Object>> res;
-    private String select;
-    private String update;
 
     public SocioDAO(DBConnector db) {
         this.db = db;
@@ -28,8 +24,8 @@ public class SocioDAO implements InterfacciaSocioDAO{
 /**metodo per ottenere il kart posseduto dall'utente*/
     @Override
     public Kart getKart() {
-        query = Query.MOSTRA_KART_SOCIO.getQuery();
-        res = db.executeReturnQuery(query);
+        String query = Query.MOSTRA_KART_SOCIO.getQuery();
+        List<Map<String, Object>>res = db.executeReturnQuery(query);
         if (res.isEmpty()) {
             return null;
         }
@@ -40,12 +36,12 @@ public class SocioDAO implements InterfacciaSocioDAO{
         return val;
     }
 
-    /**controllo della prenotazione
-    (eventualmente inglobabile sotto, bisogna vedere)*/
+/**controllo della prenotazione
+ * (eventualmente inglobabile sotto, bisogna vedere)*/
     public int checkPrenotazione(String username, LocalDate data, LocalTime intervallo , LocalDate dataCorr){
 
-        query = Query.PRENOTAZIONE_CONTEGGIO_POSTI_RIMASTI.getQuery(data, intervallo);
-        res = db.executeReturnQuery(query);
+        String query = Query.PRENOTAZIONE_CONTEGGIO_POSTI_RIMASTI.getQuery(data, intervallo);
+        List<Map<String, Object>> res = db.executeReturnQuery(query);
         String prenotazioniConcorrenti = res.get(0).get("concurrent").toString();
 
         return (prenotazioniConcorrenti.equals("20")) ? 1 : 0;
@@ -57,8 +53,8 @@ public class SocioDAO implements InterfacciaSocioDAO{
     public int prenotazione(String username, LocalDate data, LocalTime intervallo, LocalDate dataCorr){
         Socket clientSocket = null; //temp
         PHPResponseHandler responder = null; //!!!temporaneo, errore
-        query = Query.PRENOTAZIONE_MAX_ID.getQuery();
-        res = db.executeReturnQuery(query);
+        String query = Query.PRENOTAZIONE_MAX_ID.getQuery();
+        List<Map<String, Object>> res = db.executeReturnQuery(query);
         String idPrenotazione;
         idPrenotazione = res.get(0).get("max").toString();
         idPrenotazione = (!idPrenotazione.equals("0")) ? String.valueOf(Integer.parseInt(idPrenotazione) + 1) : "1";
@@ -71,7 +67,7 @@ public class SocioDAO implements InterfacciaSocioDAO{
     @Override
     public int registrazione(String nome, String cognome, LocalDate dataNascita, String cF, String mail, String password) {
 
-        query = Query.REGISTRAZIONE_SOCIO.getQuery(cF, nome, cognome, mail, password, dataNascita);
+        String query = Query.REGISTRAZIONE_SOCIO.getQuery(cF, nome, cognome, mail, password, dataNascita);
 
         String out = db.executeUpdateQuery(query);
         if(out.equals("0"))
@@ -83,15 +79,17 @@ public class SocioDAO implements InterfacciaSocioDAO{
  * @param ora ora corrente*/
     @Override
     public int acquistaKart(String targa, String cF, LocalDateTime ora) {//LocalDateTime.now());
-        update = Query.ACQUISTO_KART_UTENTE_TABELLA_SOCIO.getQuery(targa, cF);
-        select = Query.ACQUISTO_KART_UTENTE_TROVA_ID_PRODOTTO.getQuery(targa);
+        String query = Query.ACQUISTO_KART_UTENTE_TABELLA_SOCIO.getQuery(targa, cF);
 
-        String queryIndicator = db.executeUpdateQuery(update);
+
+        String queryIndicator = db.executeUpdateQuery(query);
         if (queryIndicator.equals("0")) {
             return 1;
         }
 
-        List<Map<String, Object>> idProdotto = db.executeReturnQuery(select);
+        query = Query.ACQUISTO_KART_UTENTE_TROVA_ID_PRODOTTO.getQuery(targa);
+
+        List<Map<String, Object>> idProdotto = db.executeReturnQuery(query);
         query = Query.ACQUISTO_KART_UTENTE_TABELLA_ACQUISTA.getQuery(cF, idProdotto.get(0).get("idProdotto").toString(), ora);
         queryIndicator = db.executeUpdateQuery(query);
 
@@ -101,8 +99,8 @@ public class SocioDAO implements InterfacciaSocioDAO{
 /**Metodo per ottenere i pezzi acquistabili*/
     @Override
     public List<Pezzo> ottieniPezzi() {
-        select = Query.MOSTRA_PEZZI_CONCESSIONARIA.getQuery();
-        res = db.executeReturnQuery(select);
+        String query = Query.MOSTRA_PEZZI_CONCESSIONARIA.getQuery();
+        List<Map<String, Object>> res = db.executeReturnQuery(query);
         List<Pezzo> result = null;
         for(Map<String, Object> i : res){
             result.add (
@@ -117,12 +115,9 @@ public class SocioDAO implements InterfacciaSocioDAO{
     @Override
     public int acquistaPezzi(String IDProdotto, int quantita, String username, LocalTime ora) {
 
-        update = Query.ACQUISTA_PEZZI_TABELLA_CONCESSIONARIA.getQuery(IDProdotto);
-        query = Query.ACQUISTA_PEZZI_TABELLA_ACQUISTA.getQuery(username, IDProdotto, ora);
         String[] queries = new String[2];
-        queries[0] = update;
-        queries[1] = query;
-
+        queries[0] = Query.ACQUISTA_PEZZI_TABELLA_CONCESSIONARIA.getQuery(IDProdotto);
+        queries[1] = Query.ACQUISTA_PEZZI_TABELLA_ACQUISTA.getQuery(username, IDProdotto, ora);
         String check;
 
         for (String prodotto : queries) {
